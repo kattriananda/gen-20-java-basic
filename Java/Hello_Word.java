5df0faa9f50081a5d6aecaa8b646d156da67e54c
kattriananda <161298527+kattriananda@users.noreply.github.com>
2024-02-28 00:21:24 +0700

    Penambahan Comment

--- Java/Hello_Word.java
+++ Java/Hello_Word.java
@@ -1,5 +1,11 @@
 public class Hello_Word{
+    //untuk mendeklarasikan class coding kita
+    //nama class harus sama dengan nama file java yang dibuat
     public static void main (String [] args) {
+        //Method main adalah tempat menjalankan perintah
+        //Method ini harus dimiliki semua program java
         System.out.println("Hello Word!");
+        //prin untuk menampilkan output
+        //println untuk menampilkan output setelahnya di baris baru
     }
 }
